03455121d5a8e0ff1a25851c5aa3b95926b5e6e0
Janvi Chincholkar <janvichincholkar14@gmail.com>
2024-08-08 13:11:23 +0530

    models

---
+++ server/models/Secret.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose"
+
+const userSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    } ,
+
+    /*category : {
+    type: string,
+    enum : ["personal", "professional"],
+    }*/
+   
+      user:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      }
+},
+
+    {
+        timestamps: true,
+
+    });
+
+const Secret = model("Secret", secretSchema);
+
+export default Secret;
